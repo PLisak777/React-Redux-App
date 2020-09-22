f47e88d2041a745b2c0e37c7c2580eada7774b02
Phil <philip-lisak@lambdastudents.com>
2020-09-21 20:42:08 -0700

    Initial Commit before I eff up the world

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+// import { fetchData } from './actions';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
